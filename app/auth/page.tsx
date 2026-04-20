f5ef68fcb21e1869c3c23560a65bf46f9b0c7021
vedanttiwari07 <tiwarivedant07@gmail.com>
2026-04-21 00:55:42 +0530

    Update

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from '@clerk/nextjs'
 export default function SignUpPage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <SignUp afterSignUpUrl="/" />
+      <SignUp afterSignOutUrl="/" />
     </div>
   )
 }
