b5b2f754f92262678fc53831131a21f7940bc86e
vedanttiwari07 <tiwarivedant07@gmail.com>
2026-04-21 10:28:32 +0530

    Fixed auth error

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,9 +1,11 @@
-import { SignUp } from '@clerk/nextjs'
+"use client";
+
+import { SignUp } from '@clerk/nextjs';
 
 export default function SignUpPage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <SignUp afterSignOutUrl="/" />
     </div>
-  )
+  );
 }
